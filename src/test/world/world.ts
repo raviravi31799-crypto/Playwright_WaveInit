f634f2cbe53890a6117dd2e8f1b417e28d07f092
sriram-Kannan-45 <wavene20@gmail.com>
2026-08-25 13:58:30 +0530

    feat: add negative registration scenarios (duplicate email, password mismatch, terms unchecked) and inject POM instances into CustomWorld

--- src/test/world/world.ts
+++ src/test/world/world.ts
@@ -1,11 +1,17 @@
 import { setWorldConstructor, World, IWorldOptions } from "@cucumber/cucumber";
 import { Browser, BrowserContext, Page } from "@playwright/test";
+import type { BasePage } from "../pages/base.page";
+import type { HomePage } from "../pages/home.page";
+import type { RegisterPage } from "../pages/register.page";
 import { logger } from "../utils/logger";
 
 export class CustomWorld extends World {
     browser!: Browser;
     browserContext!: BrowserContext;
     page!: Page;
+    basePage!: BasePage;
+    homePage!: HomePage;
+    registerPage!: RegisterPage;
     logger = logger;
 
     constructor(options: IWorldOptions) {
